c24903133b4eef65ca510e280bf98ea69fda715d
2001Kaye <jhan117@naver.com>
2024-08-19 17:18:32 +0900

    Feat: 인터뷰 page 완료

--- src/app/_components/blog/InterviewPage.js
+++ src/app/_components/blog/InterviewPage.js
@@ -9,15 +9,22 @@ const InterviewPage = (props) => {
             Q{idx + 1}: {props.data.questions[idx]}
           </h4>
           <div>
-            {props.data.answers[idx].map((a, aIdx) => (
-              <p>
-                <b>
-                  A{idx + 1}
-                  {props.data.answers[idx].length != 1 && `-${aIdx + 1}`}:
-                </b>{" "}
-                {a}
-              </p>
-            ))}
+            {props.data.answers &&
+              props.data.answers[idx].map((a, aIdx) =>
+                idx == 1 && aIdx == 2 ? (
+                  <p>
+                    <i>{a}</i>
+                  </p>
+                ) : (
+                  <p>
+                    <b>
+                      A{idx + 1}
+                      {props.data.answers[idx].length != 1 && `-${aIdx + 1}`}:
+                    </b>{" "}
+                    {a}
+                  </p>
+                )
+              )}
             {props.data.summary && props.data.summary[idx] && (
               <p>
                 <b>답변 요약:</b> {props.data.summary[idx]}
