cd75b0f49d405672f98392c7b3996f91399e4d53
kaye-main <jhan117@naver.com>
2026-07-09 14:59:10 +0900

    fix(data): restructure config files and fix bullet-list rendering

---
+++ src/data/configs/economy_education_config.ts
@@ -0,0 +1,88 @@
+import { PageConfig } from "../types";
+
+export const economy_education_config: PageConfig = {
+  header: "필리핀의 교육",
+  writer: "신나연",
+  blocks: [
+    {
+      type: "h3-section",
+      title: "필리핀의 교육시스템",
+      blocks: [
+        {
+          type: "h4-section",
+          title: "취학 전 과정(Pre-School)",
+          blocks: [
+            {
+              type: "paragraph",
+              text: "2014년 이전에는 초등학교 6년 고등학교 4년으로 10년의 교육과정이었습니다. 하지만 국제표준교육과정(12년의 교육과정)과는 달랐기에 필리핀 학생들이 외국 대학으로 진학할 때 12년간의 성적을 제출할 수 없어 큰 문제로 자리 잡았습니다. 또한 '인력 수출국'으로 불리는 필리핀의 입장에서는 10년제 교육제도로는 고등교육을 받은 근로자로 해외에서 인정을 받기 어려웠습니다. 이 때문에 2014년 이후 정부는 2년간의 Senior High School 과정에서 기초적인 기술교육(컴퓨터, 보건 등)을 가르치게 됩니다. 이 과정은 학교 졸업 후 바로 취업하려는 학생과 대학에 들어가기 전 사전지식을 채우고 싶은 학생들을 위한 교육과정을 제공하고 있습니다. 이를 'K-12 제도'라고 합니다.",
+            },
+            {
+              type: "image",
+              src: "/images/educationImgs/es1.png",
+            },
+          ],
+        },
+        {
+          type: "h4-section",
+          title: "초등과정(Grade School)",
+          blocks: [
+            {
+              type: "paragraph",
+              text: "취학 전 유아교육과정은 크게 2가지로 분류할 수 있습니다. 하나는 만 2세~4.5세의 유아들을 위한 과정과 다른 하나는 만4.5세~6.5세의 아이들을 위한 과정으로 한국의 유치원, 어린이집의 개념과 비슷합니다. 이를 Pre-School 또는 Kindergarten이라 부르는데, 하루에 약 4시간 정도의 수업을 받습니다.",
+            },
+          ],
+        },
+        {
+          type: "h4-section",
+          title: "중등과정(Junior High School)",
+          blocks: [
+            {
+              type: "paragraph",
+              text: "필리핀의 초등학교 교과과정은 6년이며 만 6세부터 입학이 가능합니다. 필리핀 공립학교의 경우는 거의 학비가 없거나 몇천 페소 정도이지만 외국인 학교 혹은 사립학교 같은 경우는 한 학기에 약 3~6만 페소(약 60~120만 원)를 지불하는 경우도 있습니다. 필리핀은 현지 언어와 영어를 함께 학습하여 대부분의 수업을 영어로 진행합니다. 필리핀의 초등과정 교과목을 간단히 소개하자면\n1~3학년(Grade 1-3) : English, Filipino, Mathematics, Science & Health, Computer, Sibika & Kulture, Musika, Sining, at Edukasyong Pangkatawan(MSEP), Bible 등\n4~6학년(Grade 4~6) : English, Filipino, Mathematics, Science & Health, Computer, Sibika & Kulture, Musika, Sining, at Edukasyong Pangkatawan(MSEP), Edukasyong Pantahanan at Pangkabuhayan(EPP), Bible 등\n으로 영어가 주된 교육과정 중 하나인 것을 알 수 있습니다.",
+            },
+          ],
+        },
+        {
+          type: "h4-section",
+          title: "고등과정(Senior High School)",
+          blocks: [
+            {
+              type: "paragraph",
+              text: "Junior High School의 교과과정은 4년으로 이때의 교육과정도 초등과정과 크게 다르지 않습니다. 우리나라의 중학교와 고등학교가 합해진 개념으로 생각하면 됩니다. 중등과정 교과목을 간단히 소개하자면\n1~4학년(Grade 7~10) : English, Filipino, Mathematics, Science, Computer, Home Economics & Technology, Physical Education, Health, Music(PEHM), Araling Panlipunan, Revitalized Homeroom Guidance Program(RHGP), Computer, Bible 등\n으로 초등과정의 심화적인 내용을 주로 다룹니다.",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: "h3-section",
+      title: "필리핀 교육의 문제점",
+      blocks: [
+        {
+          type: "h4-section",
+          title: "낮은 교육 수준",
+          blocks: [
+            {
+              type: "paragraph",
+              text: "객관적인 지표로 보아는 필리핀의 교육 수준은 낮은 편입니다. PISA 국제 학업 성취도 평가에서 조사 대상 77개국 가운데 76위를 기록했습니다. 하지만 2012년 개정 교육안이 통과된 이후 이는 조금 완화되나 했지만 필리핀 국민들의 부정적인 의견이 많아 아직은 평균에 못 미치는 교육 수준을 가지고 있습니다.",
+            },
+          ],
+        },
+        {
+          type: "h4-section",
+          title: "낮은 생활 수준과 대가족 체제로 인한 질적 수준 하락",
+          blocks: [
+            {
+              type: "paragraph",
+              text: "필리핀은 낮은 1인당 GDP(국내 총생산)로 인해 질적 수준도 낮으며 종교, 전통의 문제로 대가족의 체제가 태반 수를 이루고 있어 자녀들의 교육 자체를 포기하거나 장남 혹은 장녀에게만 경제적인 지원을 해주고 나머지 자녀들은 바로 취업을 시키는 경우가 많습니다. 이러한 모습은 우리나라의 1960년~1970년대의 한국과 유사한 모습을 연상시키며 필리핀은 아직 가부장제와 호주제, 남아 선호, 대가족제의 경향이 아주 짙습니다.",
+            },
+            {
+              type: "image",
+              src: "/images/educationImgs/es2.jpg",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
